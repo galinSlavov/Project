89af6f42dc610c5d380f81daf43089c235c818a5
Galin <galindiqnow@abv.bg>
2022-01-11 13:50:00 +0200

    git add

--- src/ProjectGalin.java
+++ src/ProjectGalin.java
@@ -1,3 +1,9 @@
-public class ProjectGalin {
+import java.util.Scanner;
 
+public class ProjectGalin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String boss = scanner.nextLine();
+        int bossPower = Integer.parseInt(scanner.nextLine());
+    }
 }
