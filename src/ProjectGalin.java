ba3bf7e7e0c7eff1ed70db624ac851b386b3f81c
Galin Slavov <galindiqnow@gmail.com>
2022-02-23 12:30:07 +0200

    git add

--- src/ProjectGalin.java
+++ src/ProjectGalin.java
@@ -1,15 +1,8 @@
-import java.awt.event.ItemEvent;
 import java.util.Scanner;
 
 public class ProjectGalin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int i = 1;
-        int y = 2;
-        int x = 3;
-
-        int total = i + y + x;
-        System.out.println("The result is " + total);
 
         String name = scanner.nextLine();
         String team = scanner.nextLine();
@@ -17,6 +10,13 @@ public class ProjectGalin {
 
         System.out.println("This player name is " + name +
                 ".He play in soccer team " + team +
-                " and the team is from league " +league );
+                " and the team is from league " + league);
+
+
+        String django = "gosho";
+        String anso = "ivan";
+        String unzo = "kolyo";
+
+        System.out.println(django + " " + anso + " " + unzo);
     }
 }
