ee65e1a8239ef0a247d03f5b3a379c5ab5355801
Galin Slavov <galindiqnow@gmail.com>
2022-02-23 12:54:57 +0200

    git add Data Types

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class DataTypes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+String newString = scanner.next();
+        System.out.println(newString + " " + "eat");
     }
 }
